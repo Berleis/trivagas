d8e4d04c27aed88e0ebbd59136171792f5d119b6
heellenalves <31360142+heellenalves@users.noreply.github.com>
2017-11-14 11:25:48 -0200

    Classe e cadastro - Empresa

---
+++ Classes/Empresa.php
@@ -0,0 +1,122 @@
+<?php
+
+class Empresa{
+    
+    private $id;
+    private $nome;
+    private $cnpj;
+    private $senha;
+    private $email;
+    
+    //Getters
+    public function getId() {
+        return $this->id;
+    }
+    public function getNome() {
+        return $this->nome;
+    }
+    public function getCnpj() {
+        return $this->cnpj;
+    }
+    public function getSenha() {
+        return $this->senha;
+    }
+    public function getEmail() {
+        return $this->email;
+    }
+    
+    //Setters
+    public function setId($id) {
+        $this->id = $id;
+    }
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+    public function setCnpj($cnpj) {
+        $this->cnpj = $cnpj;
+    }
+    public function setSenha($senha) {
+        $this->senha = $senha;
+    }
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+    
+    //M�todos
+    
+    public function incluir(Empresa $empresa){
+        try{
+            $link=mysqli_connect("localhost", "root", "", "trivagas");
+            $query="INSERT INTO empresas (`id`, `nome`, `cnpj`, `senha`, `email`)
+			VALUES (NULL, '$empresa->nome', '$empresa->cnpj', '$empresa->senha', '$empresa->email')";
+            mysqli_query($link, $query);
+        }catch(Exception $e){
+            echo $e;
+        }
+    }
+    
+    public function buscar($cnpj){
+        try {
+            $link=mysqli_connect("localhost", "root", "", "trivagas");
+            $query="SELECT * FROM empresas WHERE cnpj = '$cnpj'";
+            $result=mysqli_query($link, $query);
+            $obj=mysqli_fetch_object($result);
+            return $obj;
+        } catch (Exception $e) {
+            echo $e;
+        }
+    }
+    
+    public function buscarPorId($id){
+        try {
+            $link=mysqli_connect("localhost", "root", "", "trivagas");
+            $query="SELECT * FROM empresas WHERE id = '$id'";
+            $result=mysqli_query($link, $query);
+            $obj=mysqli_fetch_object($result);
+            return $obj;
+        } catch (Exception $e) {
+            echo $e;
+        }
+    }
+    
+    public function listar(){
+        try{
+            $link=mysqli_connect("localhost", "root", "", "trivagas");
+            $query = $link->query("SELECT * FROM empresas");
+            while($result = $query->fetch_object()){
+                $obj[] = $result;
+            }
+            if(empty($obj)){
+                return null;
+            }
+            return $obj;
+        } catch (Exception $e) {
+            echo $e;
+        }
+    }
+    
+    public function alterar(Empresa $empresa){
+        try{
+            $link=mysqli_connect("localhost", "root", "", "trivagas");
+            $query="UPDATE empresas SET nome = '$empresa->nome', cnpj = '$empresa->cnpj',
+			senha = '$empresa->senha', email = '$empresa->email'
+			WHERE id = '$empresa->id'";
+            mysqli_query($link, $query);
+        }catch (Exception $e){
+            echo $e;
+        }
+    }
+    
+    public function excluir($id){
+        try {
+            $link=mysqli_connect("localhost", "root", "", "trivagas");
+            $query="DELETE FROM empresas WHERE id = '$id'";
+            mysqli_query($link, $query);
+        } catch (Exception $e) {
+            echo $e;
+        }
+    }
+    
+}
+
+?>
---
+++ Empresa/Cadastrar.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+	<title>Cadastro da empresa</title>
+	<?php include '../Template/Header.php';?>
+	<?php include '../Template/Footer.php';?>
+</head>
+<body>
+	<div class="container" style="width: 30%; margin-top: 10%">
+    	<form method="post" action="#">
+    		<label>Nome</label><br>
+    		<input required placeholder="Digite o nome" name="nome" class="form-control"><br>
+    		<label>CNPJ</label><br>
+    		<input required placeholder="Digite o cnpj" name="cnpj" class="form-control"><br>
+    		<label>Senha</label><br>
+    		<input required placeholder="Crie uma senha" name="senha" class="form-control" type="password"></input><br><br>
+    		<label>Email</label><br>
+    		<input required placeholder="Digite o e-mail" name="email" class="form-control"><br>
+    		<input class="btn btn-primary" type="reset" value="Cancelar">
+    		<input class="btn btn-success" type="submit" value="Cadastrar" name="cadastrar">
+    	</form>
+    	<?php 
+    		if(isset($_POST['cadastrar'])){
+    			require_once '../Classes/Empresa.php';
+    			$empresa = new Empresa();
+    			$empresa->setNome($_POST['nome']);
+    			$empresa->setCnpj($_POST['cnpj']);
+    			$empresa->setSenha($_POST['senha']);
+    			$empresa->setEmail($_POST['email']);
+    			$row=$empresa->buscar($_POST['cnpj']);
+    			if($row == null){
+    				$empresa->incluir($empresa);
+    				header("location: Listar.php");
+    			}else{
+    				echo "<br>CNPJ já existe no sistema!";
+    			}
+    		}
+    	?>
+	</div>
+</body>
+</html>
