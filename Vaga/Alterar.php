217933b0a9a16c93a7afab36c8d497c6c375aafe
Berleis <31280249+Berleis@users.noreply.github.com>
2017-11-23 14:26:46 -0200

    Formulários das vagas
    Modificação nos formulários de cadatro e alteração das vagas

--- Vaga/Alterar.php
+++ Vaga/Alterar.php
@@ -6,24 +6,28 @@
     <?php include '../Template/Header.php';?>
 </head>
 <body>
-    <div class="container" style="width: 30%; margin-top: 8%">
+    <div class="container" style="width: 100%; margin-top: 10%">
     	<form method="post" action="#">
     		<?php 
     			require_once '../Classes/Vaga.php';
     			$v = new Vaga();
     			$v = $v->buscarPorId($_SESSION['id_vaga'])
     		?>
-    		<label>Descrição da vaga</label>
-    		<input required placeholder="Digite a descrição da vaga" name="descricao" class="form-control" value="<?php echo $v->descricao;?>"></input><br>
-    		<label>Horario</label><br>
-    		<input required placeholder="Digite o horario" name="horario" class="form-control" value="<?php echo $v->horario;?>"></input><br>
-    		<label>Salario</label><br>
-    		<input required placeholder="Digite o salário" name="salario" class="form-control" value="<?php echo $v->salario;?>"></input><br>
-			<label>Benefícios</label><br>
-    		<input required placeholder="Digite os beneficios desta vaga" name="beneficios" class="form-control" value="<?php echo $v->beneficios;?>"></input><br>
-			<label>Categoria</label><br>
-    		<input required placeholder="Digite a categoria" name="categoria" class="form-control" value="<?php echo $v->categoria;?>"></input><br>
-    		<input class="btn btn-success" type="submit" value="Salvar alterações" name="alterar">
+    		<div style="width: 35%; float: left; margin-left: 10%">
+    			<label>Descrição da vaga</label><br>
+	    		<input required placeholder="Digite a descrição da vaga" name="descricao" class="form-control" value="<?php echo $v->descricao;?>"><br>
+	    		<label>Horario</label><br>
+	    		<input required placeholder="Digite o horário" name="horario" class="form-control" value="<?php echo $v->horario;?>"><br>
+	    		<label>Salario</label><br>
+	    		<input required placeholder="Digite o salário" name="salario" class="form-control" value="<?php echo $v->salario;?>"></input><br>
+    		</div>
+    		<div style="width: 35%; float: right; margin-right: 10%">
+    			<label>Beneficios</label><br>
+	    		<textarea required placeholder="Digite os beneficios desta vaga" name="beneficios" class="form-control"> <?php echo $v->beneficios;?></textarea><br>
+				<label>Categoria</label><br>
+	    		<input required placeholder="Digite a categoria" name="categoria" class="form-control" value="<?php echo $v->categoria;?>"></input><br><br>			
+	    		<input class="btn btn-success" type="submit" value="Salvar alterações" name="alterar" style="float: right">
+    		</div>	
     		<?php 
     			if(isset($_POST['alterar'])){
     			    $v = new Vaga();
--- Vaga/Cadastrar.php
+++ Vaga/Cadastrar.php
@@ -12,20 +12,24 @@
     ?>
 </head>
 <body>
-	<div class="container" style="width: 30%; margin-top: 5%">
+	<div class="container" style="width: 100%; margin-top: 10%">
     	<form method="post" action="#">
-    		<label>Descrição da vaga</label><br>
-    		<input required placeholder="Digite a descrição da vaga" name="descricao" class="form-control"><br>
-    		<label>Horario</label><br>
-    		<input required placeholder="Digite o horário" name="horario" class="form-control"><br>
-    		<label>Salario</label><br>
-    		<input required placeholder="Digite o salário" name="salario" class="form-control"></input><br>
-			<label>Beneficios</label><br>
-    		<input required placeholder="Digite os beneficios desta vaga" name="beneficios" class="form-control"></input><br>
-			<label>Categoria</label><br>
-    		<input required placeholder="Digite a categoria" name="categoria" class="form-control"></input><br><br>			
-    		<input class="btn btn-primary" type="reset" value="Cancelar">
-    		<input class="btn btn-success" type="submit" value="Cadastrar" name="cadastrar">
+    		<div style="width: 35%; float: left; margin-left: 10%">
+    			<label>Descrição da vaga</label><br>
+	    		<input required placeholder="Digite a descrição da vaga" name="descricao" class="form-control"><br>
+	    		<label>Horario</label><br>
+	    		<input required placeholder="Digite o horário" name="horario" class="form-control"><br>
+	    		<label>Salario</label><br>
+	    		<input required placeholder="Digite o salário" name="salario" class="form-control"></input><br>
+    		</div>
+    		<div style="width: 35%; float: right; margin-right: 10%">
+    			<label>Beneficios</label><br>
+	    		<textarea required placeholder="Digite os beneficios desta vaga" name="beneficios" class="form-control"></textarea><br>
+				<label>Categoria</label><br>
+	    		<input required placeholder="Digite a categoria" name="categoria" class="form-control"></input><br><br>			
+	    		<input class="btn btn-success" type="submit" value="Cadastrar" name="cadastrar" style="float: right">
+	    		<input class="btn btn-primary" type="reset" value="Cancelar" style="float: right">
+    		</div>			
     	</form>
     	<?php 
     		if(isset($_POST['cadastrar'])){
