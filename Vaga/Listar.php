0d0dfc6baa16473b003d42206ec2cce1c656d0d5
Berleis <31280249+Berleis@users.noreply.github.com>
2017-11-23 14:13:56 -0200

    Lista de vagas
    Modficação na exibição das vagas para os usuários

--- Vaga/Listar.php
+++ Vaga/Listar.php
@@ -20,7 +20,6 @@
 <body>
 	<div class="container" style="width: 80%; margin-top: 8%">
     	<?php
-    	
     		require_once '../Classes/Vaga.php';
     		$vaga = new Vaga();
     		$vagas = $vaga->listar();
@@ -29,41 +28,54 @@
     		}else{?>
     		<table class="table table-bordered table-stripped table-hover">
     			<tr>
-    				<th>Id</th>
+    				<th>Código</th>
     				<th>Descrição</th>
-    				<th>Horário</th>
     				<th>Salário</th>
-					<th>Beneficios</th>
-    				<th>Categoria</th>
-    				<th>Alterar</th>
-    				<th>Excluir</th>
+    				<th>Empresa</th>
+    				<th>Detalhes</th>
     			</tr>
     			<?php 
     			
     				foreach ($vagas as $v){
+    					require_once '../Classes/Empresa.php';
+    					$e = new Empresa();
+    					$e = $e->buscar($v->id_empresa);
     					echo '<tr>';
     					echo	'<td>'.$v->id.'</td>';
     					echo	'<td>'.$v->descricao.'</td>';
-    					echo	'<td>'.$v->horario.'</td>';
     					echo	'<td>'.$v->salario.'</td>';
-						echo	'<td>'.$v->beneficios.'</td>';
-    					echo	'<td>'.$v->categoria.'</td>';
-    					echo	'<td><a href="Listar.php?alterar&id='.$v->id.'">Alterar</a></td>';
-    					echo	'<td><a href="Listar.php?excluir&id='.$v->id.'">Excluir</a></td>';
+    					echo	'<td>'.$e->nome.'</td>';
+    					echo 	'<td><a data-toggle="modal" href="#modalVaga'.$v->id.'">Detalhes</a></td>';
     					echo '</tr>';
-    				}
-    				if(isset($_GET['alterar'])){
-    					session_start();
-    					$_SESSION['id_vaga'] = $_GET['id'];
-    					header('location: Alterar.php');
-    				}
-    				if(isset($_GET['excluir'])){
-    					$vaga->excluir($_GET['id']);
-    					exit(header('location: Listar.php'));
     				}
     			?>
     		</table>
     	<?php }?>
+    	<?php if($vagas != null){ foreach($vagas as $v){?>
+			<div id="modalVaga<?php echo $v->id?>" class="modal fade" role="dialog">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal">&times;</button>
+							<h4 class="modal-title">Detalhes</h4>
+						</div>
+						<div class="modal-body">
+							<?php
+								echo '<br>Código: '.$v->id;
+								echo '<br>Descrição: '.$v->descricao;
+								echo '<br>Horário: '.$v->horario;
+								echo '<br>Benefícios: '.$v->beneficios;
+								echo '<br>Categoria: '.$v->categoria;
+								echo '<br>Salário: '.$v->salario;
+							?>
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		<?php }}?>
 	</div>
 </body>
 </html>
